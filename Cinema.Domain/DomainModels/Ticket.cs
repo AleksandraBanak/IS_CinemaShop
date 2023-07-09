87993592e9ecc48adc066f5170c47ee15bf3ebd5
Aleksandra <aleksandrapetkovskabanak@gmail.com>
2023-07-09 22:59:08 +0200

    First commit

---
+++ Cinema.Domain/DomainModels/Ticket.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Cinema.Domain.DomainModels
+{
+    public class Ticket : BaseEntity
+    {
+       
+        [Required]
+        [Display(Name = "Movie Name")]
+        public string Title { get; set; }
+        [Required]
+        [Display(Name = "Movie ticket price")]
+        public int TicketPrice { get; set; }
+        [Required]
+        [Display(Name = "Movie date")]
+        public DateTime Date { get; set; }
+        public virtual ICollection<TicketInShoppingCart> TicketInShoppingCarts { get; set; }
+        public virtual ICollection<TicketInOrder> Orders { get; set; }
+    }
+}
